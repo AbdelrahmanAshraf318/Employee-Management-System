45e113caa04f51c808e959e7df475d6dde3e5beb
AbdelrahmanAshraf318 <abdelrahmanashraf25102000@gmail.com>
2025-04-05 00:28:16 +0200

    Apply Password-Based Authentication

--- Employee Management System/Employee-Management-System/src/main/java/com/example/Employee/Management/System/security/ApplicationConfigSecurity.java
+++ Employee Management System/Employee-Management-System/src/main/java/com/example/Employee/Management/System/security/ApplicationConfigSecurity.java
@@ -4,9 +4,11 @@ package com.example.Employee.Management.System.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,32 +25,39 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ApplicationConfigSecurity
 {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final CustomUserDetailsService userDetailsService;
+
+    public ApplicationConfigSecurity(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-    {
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest()
-                .authenticated());
-        //http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        // To make your http stateless
-        http.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/hello").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider());
 
         return http.build();
     }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider()
-    {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(new BCryptPasswordEncoder());
-        provider.setUserDetailsService(userDetailsService);
-        return provider;
-    }
-
 
 }
--- Employee Management System/Employee-Management-System/src/main/java/com/example/Employee/Management/System/service/UserService.java
+++ Employee Management System/Employee-Management-System/src/main/java/com/example/Employee/Management/System/service/UserService.java
@@ -4,9 +4,11 @@ package com.example.Employee.Management.System.service;
 import com.example.Employee.Management.System.models.User;
 import com.example.Employee.Management.System.repository.UserRepo;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,9 +21,22 @@ public class UserService
 {
     private final UserRepo userRepo;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public UserService(UserRepo userRepo)
     {
         this.userRepo = userRepo;
+    }
+
+
+
+    public void registerUser(User user)
+    {
+        // Encode the raw password
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userRepo.save(user);
     }
 
     public User findById(UUID id)
