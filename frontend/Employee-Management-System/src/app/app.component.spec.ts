f0a4ed4214fad34a8387f1f3a2a79ec68b6da8c9
AbdelrahmanAshraf318 <abdelrahmanashraf25102000@gmail.com>
2025-04-21 23:25:20 +0200

    Add Frontend Layer Dependencies

--- frontend/Employee-Management-System/src/app/app.component.spec.ts
+++ frontend/Employee-Management-System/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Employee-Management-System'`, () => {
+  it(`should have as title 'employee-management-system'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Employee-Management-System');
+    expect(app.title).toEqual('employee-management-system');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Employee-Management-System app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('employee-management-system app is running!');
   });
 });
